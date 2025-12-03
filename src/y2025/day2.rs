ac1fc91a7a15fda810735cc8d3a23a45e54a6f06
Alex Lodge <awlodge@gmail.com>
2025-12-03 21:11:44 +0000

    2025 day 2 part 2 (slow)

--- src/y2025/day2.rs
+++ src/y2025/day2.rs
@@ -5,7 +5,7 @@ use crate::helpers::parse::parse_split;
 const INPUT: &str = include_str!("input/day2.txt");
 
 pub fn run() -> (u64, u64) {
-    (sum_invalid_ids(INPUT), 0)
+    (sum_invalid_ids(INPUT), sum_really_invalid_ids(INPUT))
 }
 
 fn parse(input: &str) -> impl Iterator<Item = ProductIdRange> {
@@ -14,6 +14,14 @@ fn parse(input: &str) -> impl Iterator<Item = ProductIdRange> {
 
 fn sum_invalid_ids(input: &str) -> u64 {
     parse(input).map(|r| r.list_invalid_ids()).flatten().sum()
+}
+
+fn sum_really_invalid_ids(input: &str) -> u64 {
+    let mut s: u64 = 0;
+    for r in parse(input) {
+        s += r.list_really_invalid_ids().sum::<u64>();
+    }
+    s
 }
 
 fn count_digits(inp: u64) -> u32 {
@@ -111,13 +119,35 @@ impl ProductIdRange {
 
         res
     }
+
+    fn list_really_invalid_ids(&self) -> impl Iterator<Item = u64> {
+        (self.min..(self.max + 1)).filter(|x| is_invalid_id(*x))
+    }
+}
+
+fn digits(x: u64) -> Vec<char> {
+    x.to_string().as_str().chars().collect()
+}
+
+fn is_invalid_id(x: u64) -> bool {
+    let digits = digits(x);
+    let mut y = digits.clone();
+    y.extend_from_slice(&digits);
+    let mut idx: usize = 1;
+    while idx < digits.len() {
+        if y[idx..(idx + digits.len())] == digits {
+            return true;
+        }
+        idx += 1;
+    }
+    return false;
 }
 
 #[cfg(test)]
 mod test {
     use rstest::rstest;
 
-    use crate::y2025::day2::{ProductIdRange, run, sum_invalid_ids};
+    use crate::y2025::day2::{ProductIdRange, run, sum_invalid_ids, sum_really_invalid_ids};
 
     const TEST_INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
@@ -140,9 +170,32 @@ mod test {
         assert_eq!(1227775554, sum_invalid_ids(TEST_INPUT));
     }
 
+    #[rstest]
+    #[case("11-22", &[11, 22])]
+    #[case("95-115", &[99, 111])]
+    #[case("998-1012", &[999, 1010])]
+    #[case("1188511880-1188511890", &[1188511885])]
+    #[case("222220-222224", &[222222])]
+    #[case("1698522-1698528", &[])]
+    #[case("446443-446449", &[446446])]
+    #[case("38593856-38593862", &[38593859])]
+    #[case("565653-565659", &[565656])]
+    #[case("824824821-824824827", &[824824824])]
+    #[case("2121212118-2121212124", &[2121212121])]
+    fn test_list_really_invalid_ids(#[case] range: ProductIdRange, #[case] expected: &[u64]) {
+        let actual: Vec<u64> = range.list_really_invalid_ids().collect();
+        assert_eq!(expected, &actual);
+    }
+
+    #[test]
+    fn test_sum_really_invalid_ids() {
+        assert_eq!(4174379265, sum_really_invalid_ids(TEST_INPUT));
+    }
+
     #[test]
     fn test_solution() {
-        let (s1, _) = run();
+        let (s1, s2) = run();
         assert_eq!(13919717792, s1);
+        assert_eq!(14582313461, s2);
     }
 }
